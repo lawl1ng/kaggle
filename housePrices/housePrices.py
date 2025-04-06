a12bdcbb03026b6585bfa2c229c93a69d3fa25f5
lawl1ng <199223655+lawl1ng@users.noreply.github.com>
2025-04-06 16:16:53 +0700

    Adding and renaming files. Submission added Good file structure. Submission needed for kaggle

--- housePrices/housePrices.py
+++ housePrices/housePrices.py
@@ -2,18 +2,20 @@ import pandas as pd
 import numpy as np
 
 # Load data
-house_price_file_path = './housePrices/train.csv'
-house_price_data = pd.read_csv(house_price_file_path)
+train_data_file_path = './housePrices/train.csv'
+train_data = pd.read_csv(train_data_file_path)
+test_data_file_path = './housePrices/test.csv'
+test_data = pd.read_csv(test_data_file_path)
 # Data exploration
-house_price_data.shape
-house_price_data.head()
-house_price_data.describe()
-house_price_data.columns
+train_data.shape
+train_data.head()
+train_data.describe()
+train_data.columns
 # Do not filter rows with missing values since every row has missing values
 # house_price_data = house_price_data.dropna(axis=0)
 # Choose target and features - for now features are non categorical - need to learn how to handle categorical
-y = house_price_data.SalePrice
-X = house_price_data.select_dtypes(include=np.number)
+y = train_data.SalePrice
+X = train_data.select_dtypes(include=np.number)
 X.drop('SalePrice', axis=1, inplace=True)
 X.shape
 X.describe()
@@ -21,17 +23,17 @@ X.head()
 # Decision Tree Model
 from sklearn.tree import DecisionTreeRegressor
 # Define model. Specify a number for random_state to ensure same results each run
-house_price_model = DecisionTreeRegressor(random_state=1)
+train_model = DecisionTreeRegressor(random_state=1)
 # Fit model and test for first 5 houses
-house_price_model.fit(X, y)
+train_model.fit(X, y)
 print("Making predictions for the following 5 houses:")
 print(X.head())
 print("The predictions are")
-print(house_price_model.predict(X.head()))
+print(train_model.predict(X.head()))
 # Validate the model
 from sklearn.metrics import mean_absolute_error
-predicted_home_prices = house_price_model.predict(X)
-mean_absolute_error(y, predicted_home_prices)
+predicted_train = train_model.predict(X)
+mean_absolute_error(y, predicted_train)
 
 # Using non in-sample scores
 from sklearn.model_selection import train_test_split
@@ -41,11 +43,11 @@ from sklearn.model_selection import train_test_split
 # run this script.
 train_X, val_X, train_y, val_y = train_test_split(X, y,random_state = 0)
 # Define model
-house_price_model = DecisionTreeRegressor()
+train_model = DecisionTreeRegressor()
 # Fit model
-house_price_model.fit(train_X, train_y)
+train_model.fit(train_X, train_y)
 # get predicted prices on validation data
-val_predictions = house_price_model.predict(val_X)
+val_predictions = train_model.predict(val_X)
 print(mean_absolute_error(val_y, val_predictions)) # Really bad model - mae is about 25600
 
 # Underfitting and Overfitting
@@ -68,17 +70,26 @@ for max_leaf_nodes in [10, 30, 50, 75]:
     print("Max leaf nodes: %d  \t\t Mean Absolute Error:  %d" %(max_leaf_nodes, my_mae))
 # looks like it gets better at 75, go that direction
 
-for max_leaf_nodes in range(60, 80):
+for max_leaf_nodes in range(60, 75):
     my_mae = get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y)
     print("Max leaf nodes: %d  \t\t Mean Absolute Error:  %d" %(max_leaf_nodes, my_mae))
 # best score is 68 or 69 nodes ... but the curve isn't as simple as low point then back up ... it wavers
+
+# creating the final model
+final_model = DecisionTreeRegressor(max_leaf_nodes = 68, random_state = 1)
+final_model.fit(X, y)
+test_data = test_data.select_dtypes(include=np.number)
+predictions = final_model.predict(test_data)
+output = pd.DataFrame({'Id': test_data.Id, 'SalePrice': predictions})
+output.to_csv('./housePrices/submission.csv', index=False)
 
 # try random forest model
 from sklearn.ensemble import RandomForestRegressor
 
 forest_model = RandomForestRegressor(random_state=1)
 forest_model.fit(train_X, train_y)
-house_price_preds = forest_model.predict(val_X)
-print(mean_absolute_error(val_y, house_price_preds)) # this is better again!
+train_preds = forest_model.predict(val_X)
+print(mean_absolute_error(val_y, train_preds)) # this is better again!
 
-
+# try on test data
+train_preds = forest_model.predict(test_data)
